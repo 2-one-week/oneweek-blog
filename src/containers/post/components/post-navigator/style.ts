1382e19027c3357ed0f8b278fecd688d90305da2
2-one-week <dlgkswn885@korea.ac.kr>
2021-06-05 02:15:47 +0900

    Style change post navigtion style

--- src/containers/post/components/post-navigator/style.ts
+++ src/containers/post/components/post-navigator/style.ts
@@ -1,6 +1,7 @@
 import styled from 'styled-components';
 
 const Navigation = styled.ul`
+  width: 100%;
   margin: 40px 0;
   display: flex;
   flex-wrap: wrap;
@@ -11,18 +12,20 @@ const Navigation = styled.ul`
   li {
     border: 1px solid ${({ theme: { colors } }) => colors.mainColor};
     border-radius: 6px;
+    margin: 5px 0;
 
     &:first-child {
       padding: 6px 10px;
+      text-align: start;
     }
     &:last-child {
       padding: 6px 10px;
+      text-align: end;
     }
 
     a {
       font-size: 15px;
       opacity: 0.8;
-      text-align: center;
     }
 
     &:hover {
